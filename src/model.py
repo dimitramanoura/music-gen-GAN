07080e4fa63d87d1f98a2ffff3ae7f7e8fa72d08
Dimitra Manoura <79058732+dimitramanoura@users.noreply.github.com>
2025-04-10 18:09:34 +0300

    Update model.py

--- src/model.py
+++ src/model.py
@@ -1,43 +1,82 @@
 import torch
 import torch.nn as nn
 
-# Generator Network
+# Number of channels in the training images
+nc = 1
+# Size of z latent vector (i.e. size of generator input)
+nz = 100
+# Size of feature maps in generator
+ngf = 64
+# Size of feature maps in discriminator
+ndf = 16
+# Number of GPUs available
+ngpu = 1
+
+# Custom weights initialization
+def weights_init(m):
+    classname = m.__class__.__name__
+    if classname.find('Conv') != -1:
+        nn.init.normal_(m.weight.data, 0.0, 0.02)
+    elif classname.find('BatchNorm') != -1:
+        nn.init.normal_(m.weight.data, 1.0, 0.02)
+        nn.init.constant_(m.bias.data, 0)
+
+# Generator class
 class Generator(nn.Module):
-    def __init__(self, latent_dim=100, output_shape=(128, 128)):
+    def __init__(self, ngpu):
         super(Generator, self).__init__()
-        self.model = nn.Sequential(
-            nn.Linear(latent_dim, 256),
-            nn.ReLU(),
-            nn.Linear(256, 512),
-            nn.ReLU(),
-            nn.Linear(512, output_shape[0] * output_shape[1]),
+        self.ngpu = ngpu
+        self.main = nn.Sequential(
+            nn.ConvTranspose2d(nz, ngf * 16, 4, 1, 0, bias=False),
+            nn.BatchNorm2d(ngf * 16),
+            nn.ReLU(True),
+            nn.ConvTranspose2d(ngf * 16, ngf * 8, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(ngf * 8),
+            nn.ReLU(True),
+            nn.ConvTranspose2d(ngf * 8, ngf * 4, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(ngf * 4),
+            nn.ReLU(True),
+            nn.ConvTranspose2d(ngf * 4, ngf * 2, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(ngf * 2),
+            nn.ReLU(True),
+            nn.ConvTranspose2d(ngf * 2, ngf, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(ngf),
+            nn.ReLU(True),
+            nn.ConvTranspose2d(ngf, nc, 4, 2, 1, bias=False),
             nn.Tanh()
         )
-        self.output_shape = output_shape
+    
+    def forward(self, input):
+        return self.main(input)
 
-    def forward(self, z):
-        img = self.model(z)
-        return img.view(-1, 1, self.output_shape[0], self.output_shape[1])
-
-# Discriminator Network
+# Discriminator class
 class Discriminator(nn.Module):
-    def __init__(self, input_shape=(128, 128)):
+    def __init__(self, ngpu):
         super(Discriminator, self).__init__()
-        self.model = nn.Sequential(
-            nn.Linear(input_shape[0] * input_shape[1], 512),
-            nn.ReLU(),
-            nn.Linear(512, 256),
-            nn.ReLU(),
-            nn.Linear(256, 1),
+        self.ngpu = ngpu
+        self.main = nn.Sequential(
+            nn.Conv2d(nc, ndf, 4, stride=2, padding=1, bias=False),
+            nn.LeakyReLU(0.2, inplace=True),
+            nn.Conv2d(ndf, ndf * 2, 4, stride=2, padding=1, bias=False),
+            nn.BatchNorm2d(ndf * 2),
+            nn.LeakyReLU(0.2, inplace=True),
+            nn.Conv2d(ndf * 2, ndf * 4, 4, stride=2, padding=1, bias=False),
+            nn.BatchNorm2d(ndf * 4),
+            nn.LeakyReLU(0.2, inplace=True),
+            nn.Conv2d(ndf * 4, ndf * 8, 4, stride=2, padding=1, bias=False),
+            nn.BatchNorm2d(ndf * 8),
+            nn.LeakyReLU(0.2, inplace=True),
+            nn.Conv2d(ndf * 8, ndf * 16, 4, stride=2, padding=1, bias=False),
+            nn.BatchNorm2d(ndf * 16),
+            nn.LeakyReLU(0.2, inplace=True),
+            nn.Conv2d(ndf * 16, 1, 4, stride=1, padding=0, bias=False),
             nn.Sigmoid()
         )
-
-    def forward(self, img):
-        img_flat = img.view(img.size(0), -1)
-        return self.model(img_flat)
+    
+    def forward(self, input):
+        return self.main(input)
 
 if __name__ == "__main__":
-    # Test model initialization
     generator = Generator()
     discriminator = Discriminator()
     print("Generator and Discriminator initialized successfully!")
